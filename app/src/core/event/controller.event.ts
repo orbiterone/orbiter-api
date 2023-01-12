2a90b65720b54aa9fd7afb61848602207f2706ef
SergeyPish <seregapish@gmail.com>
2023-01-12 12:46:02 +0200

    FIX | FIX dublicate user tokens

--- app/src/core/event/controller.event.ts
+++ app/src/core/event/controller.event.ts
@@ -64,7 +64,7 @@ export class ControllerEvent extends EventService {
                     collateral,
                   },
                 },
-                { upsert: true, new: true },
+                { new: true },
               );
 
             await this.transactionService.transactionRepository.transactionCreate(
